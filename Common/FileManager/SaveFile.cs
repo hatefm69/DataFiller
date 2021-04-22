91283078db49088ccd7253547c1a9ad62b275815
Hatef <Hatef@DESKTOP-A8DOBQG>
2021-04-22 08:49:53 +0430

    Add project files.

---
+++ Common/FileManager/SaveFile.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+namespace Common.FileManager
+{
+    public static class SaveFile
+    {
+        public static void SaveStreamAsFile(string filePath, Stream inputStream, string fileName)
+        {
+            DirectoryInfo info = new DirectoryInfo(filePath);
+            if (!info.Exists)
+            {
+                info.Create();
+            }
+
+            string path = Path.Combine(filePath, fileName);
+            using (FileStream outputFileStream = new FileStream(path, FileMode.Create))
+            {
+                inputStream.CopyTo(outputFileStream);
+            }
+        }
+        public static void Compress(string inFilePath,string outFilePath)
+        {
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "Compress.exe",
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    RedirectStandardInput = true,
+                    CreateNoWindow = true,
+                }
+            };
+            var str = $" {inFilePath} {outFilePath}";
+            process.StartInfo.Arguments = str;
+            process.Start();
+            process.WaitForExit();
+        }
+    }
+}
---
+++ DataFiller/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataFiller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
