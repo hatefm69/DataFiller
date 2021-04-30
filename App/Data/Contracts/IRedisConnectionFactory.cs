04bc88b45f626ad8c15825ed986782a8f4f7866e
Hatef <Hatef@DESKTOP-A8DOBQG>
2021-04-30 18:04:41 +0430

    clean

--- App/Data/Contracts/IRedisConnectionFactory.cs
+++ App/Data/Contracts/IRedisConnectionFactory.cs
@@ -1,12 +1,7 @@
 ﻿using ServiceStack.Redis;
-using System.Data;
 
 namespace Data
 {
-    public interface ISqlConnectionFactory
-    {
-        IDbConnection GetOpenConnection();
-    }
     public interface IRedisConnectionFactory
     {
         RedisClient GetOpenConnection();
